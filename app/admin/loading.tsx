fa8169b230fefcdcb42f6087f32120f152da3b9d
sahid <144352266+SahidDulce@users.noreply.github.com>
2025-06-12 00:15:07 -0300

    creacion de carpetas

---
+++ app/admin/loading.tsx
@@ -0,0 +1,5 @@
+import DashboardSkeleton from '@/app/uiold/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
---
+++ app/admin/products/create/page.tsx
@@ -0,0 +1,23 @@
+import Form from '@/app/uiold/invoices/create-form';
+import Breadcrumbs from '@/app/uiold/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+  const customers = await fetchCustomers();
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
